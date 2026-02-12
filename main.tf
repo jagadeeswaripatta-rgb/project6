08c28ef198766d11ff9f9c107697edb787ccb2db
jagadeeswaripatta-rgb <jagadeeswaripatta@gmail.com>
2026-02-12 06:17:56 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+
+  region = "ap-south-1"
+
+}
+
+
+
+resource "aws_s3_bucket" "project6_bucket" {
+
+  bucket = "jagadeeswari-bucket-project6"
+
+
+
+  tags = {
+
+    Name        = "Project6Bucket"
+
+    Environment = "Dev"
+
+  }
+
+}
+
+
